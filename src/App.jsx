5f9e6868312a0482533140e00420a90f3b2f1fda
ccleavinger <96743974+ccleavinger@users.noreply.github.com>
2023-12-10 13:50:38 -0600

    Added vercel stuf
    read title

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import { BrowserRouter } from "react-router-dom";
 
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components'
-import Earth from "./components/canvas/Earth.jsx";
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components';
+import { inject } from "@vercel/analytics";
+import { injectSpeedInsights } from '@vercel/speed-insights';
 
 const App = () => {
+
+  inject();
+  injectSpeedInsights();
+
   return (
     <BrowserRouter>
         <div className="relative z-0 bg-primary">
