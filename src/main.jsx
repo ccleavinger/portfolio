87e2ab7cc69aba6b4981f365c428e4015361049c
ccleavinger <96743974+ccleavinger@users.noreply.github.com>
2023-12-10 14:04:10 -0600

    Removed vercel stuf
    It broke the site.

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
-import {inject} from "@vercel/analytics";
-import {injectSpeedInsights} from "@vercel/speed-insights";
-
-
-inject();
-injectSpeedInsights();
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
