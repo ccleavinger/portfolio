21c2b48017562e1e51abd217a1796a08b21a353c
ccleavinger <96743974+ccleavinger@users.noreply.github.com>
2024-02-19 13:31:33 -0600

    next js was a mistake

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,42 +1,42 @@
-const logo = "logo_no_bg.svg";
-const backend = "backend.png";
-const creator = "creator.png";
-const mobile = "mobile.png";
-const web = "web.png";
-const github = "github.png";
-const menu = "menu.svg";
-const close = "close.svg";
+import logo from "./logo_no_bg.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
 
-const git = "tech/git.png";
-const reactjs = "tech/reactjs.png";
-const threejs = "tech/threejs.svg";
+import git from "./tech/git.png";
+import reactjs from "./tech/reactjs.png";
+import threejs from "./tech/threejs.svg";
 
-const meta = "company/meta.png";
-const shopify = "company/shopify.png";
-const starbucks = "company/starbucks.png";
-const tesla = "company/tesla.png";
+import meta from "./company/meta.png";
+import shopify from "./company/shopify.png";
+import starbucks from "./company/starbucks.png";
+import tesla from "./company/tesla.png";
 
-const carrent = "carrent.png";
-const jobit = "jobit.png";
-const tripguide = "tripguide.png";
-const neural = "NeuralNet.svg"
-const blender = "blender_logo.png"
-const coding_school = "codingschool.png"
-const coderSchool = "thecoderschool.png"
-const vulkan = "VulkanCompute.png"
-const rtx = "raytracing.png"
-const bleed = "Bleed_Hello.png"
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+import neural from "./NeuralNet.svg"
+import blender from "./blender_logo.png"
+import coding_school from "./codingschool.png"
+import coderSchool from "./thecoderschool.png"
+import vulkan from "./VulkanCompute.png"
+import rtx from "./raytracing.png"
+import bleed from "./Bleed_Hello.png"
 
-const torch = 'tech/pytorch.png'
-const python = 'tech/python.png'
-const tf = 'tech/tf.png'
-const kt = 'tech/kotlin.png'
-const gd = 'tech/godot.png'
-const dna = 'tech/dna.png'
-const cpp = 'tech/cpp.png'
-const ue5 = 'tech/ue5.png'
-const rust = 'tech/rustacean-flat-noshadow.png'
-const javascript = 'tech/javascript.png'
+import torch from './tech/pytorch.png'
+import python from './tech/python.png'
+import tf from './tech/tf.png'
+import kt from './tech/kotlin.png'
+import gd from './tech/godot.png'
+import dna from './tech/dna.png'
+import cpp from './tech/cpp.png'
+import ue5 from './tech/ue5.png'
+import rust from './tech/rustacean-flat-noshadow.png'
+import javascript from './tech/javascript.png'
 
 export {
   logo,
---
+++ src/components/About.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { Tilt } from "react-tilt"
+import { motion } from 'framer-motion'
+
+import {styles} from "../styles.js"
+import { services } from "../constants"
+import { fadeIn, textVariant } from "../utils/motion.js";
+import { SectionWrapper } from "../hoc/index.js"
+
+const ServiceCard = ({index, title, icon}) => {
+    return(
+        <Tilt className="xs:w-[250px] w-full">
+            <motion.div
+            variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+            className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+            >
+                <div
+                options={{
+                    max: 45,
+                    scale: 1,
+                    speed: 450
+                }}
+                className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[250px] flex justify-evenly items-center flex-col"
+                >
+                    <img src={icon} alt={title} className="w-16 h-16 object-contain"/>
+                    <h3 className="text-white text-[20px] font-bold text-center">{title}</h3>
+                </div>
+            </motion.div>
+        </Tilt>
+    )
+}
+
+const About = () => {
+  return(
+      <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p variants={fadeIn("", "", 0.1, 0.1)}
+      className="mt-4  text-secondary text-[17px] max-w-5xl leading-[30px]">
+          I'm currently a tutor with a love for computer science, AI, and Genetic Engineering. I have experience in
+          low-level development (Rust, C++, Vulkan, & Zig), AI/ML (Scikit Learn, PyTorch, TF/Keras, & OpenCV),
+          Game Development (Godot, C#, C++, Pygame, & Unreal Engine), Quantum Computing (Q# & Microsoft Quantum Dev Kit)
+          , and Web Development (Three.js, TailwindCSS, & JS).
+          I have been editing DNA since I was 13 and have found a great love for Genetics, Microbiology, and Immunotherapy through my education.
+      </motion.p>
+
+        <div className="mt-20 flex flex-wrap gap-10">
+            {services.map((service, index) => (
+                <ServiceCard key={service.title} index={index} {...service}/>
+            ))}
+
+        </div>
+    </>
+  )
+}
+
+export default SectionWrapper(About, "about");
